e049d27efa6c1d33da7dd4b41a49c816faf9535a
Steven Lipton <steve@makeapppie.com>
2020-08-18 14:08:32 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  ForEachDemo
+//
+// An exercise file for iOS Development Tips Weekly
+// A weekly course on LinkedIn Learning for iOS developers
+//  Season 11 (Q3 2020) video 10
+//  by Steven Lipton (C)2020, All rights reserved
+// Learn more at https://linkedin-learning.pxf.io/YxZgj
+//This Week:  Learn how to save screen space on large lists with outline views.
+//  For more code, go to http://bit.ly/AppPieGithub
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var menu = MenuModel.menu
+    var body: some View {
+
+        VStack {
+            Image("surfGirl1_250w")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+//MARK: List
+            List(menu){ item in
+                MenuRowView(item: item)
+            }
+//MARK:ForEach
+//            ScrollView{
+//                    ForEach( menu,){ item in
+//                        MenuRowView(item: item)
+//                    }
+//            }
+            Spacer()
+        }.edgesIgnoringSafeArea([.top,.bottom])
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Shared/OutlineViewDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  OutlineViewDemoApp.swift
+//  Shared
+//
+//  Created by Steven Lipton on 7/3/20.
+//
+
+import SwiftUI
+
+@main
+struct OutlineViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
